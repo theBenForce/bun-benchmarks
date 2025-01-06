63bad35aac80b2b91379b3db5149aae29b001d3e
Ben Force <benforce@gmail.com>
2025-01-06 00:09:38 -0600

    docs: update readme

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t bun-benchmark .
-
-printf "\n"
-printf ".: Running Benchmarks :."
-docker run -it -v ./:/project bun-benchmark bash -c "cd /project && pnpm run benchmark"
